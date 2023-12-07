6d9e905b97f6dc888e70f7913242e0fc6c70c339
THISISJUSTICE <yulop64@naver.com>
2023-12-07 12:43:54 +0900

    메모리 배리
    코드 재배치 억제
    쓰레드에서 Store가 이루어질 때의 커밋(가시성)

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,23 +4,54 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
-    class Program {
+    class Program
+    {
+        //메모리 배리어
+        //- 코드 재배치 억제
+        //- 가시성
+        static int x = 0;
+        static int y = 0;
+        static int r1 = 0;
+        static int r2 = 0;
+
+        static void Thread1()
+        {
+            y = 1; //Store y
+
+            //-----------------------------
+            Thread.MemoryBarrier(); //멀티 스레드에서만 적용된 값을 커밋하는 역할도 겸함(Store의 커밋)
+            r1 = x; //Load x
+        }
+
+        static void Thread2()
+        {
+            x = 1; //Store x
+
+            //-----------------------------
+            Thread.MemoryBarrier();
+            r2 = y; //Load y
+        }
+
         static void Main(string[] args)
         {
-            int[,] arr = new int[10000, 10000];
-
+            int count = 0;
+            while (true)
             {
-                for (int i = 0; i < 10000; i++)
-                    for (int j = 0; j < 10000; j++) arr[i, j] = 1;
+                count++;
+                x = y = r1 = r2 = 0;
+
+                Task t1 = new Task(Thread1);
+                Task t2 = new Task(Thread2);
+                t1.Start();
+                t2.Start();
+
+                Task.WaitAll(t1, t2);
+
+                //메모리 배리어를 하지 않으면 코드의 순서가 바뀌는 경우가 있을 수 있어 해당 사항이 나올 수 있음
+                if (r1 == 0 && r2 == 0) break;
             }
 
-            // 캐시의 공간 지역성으로 인해 위의 경우가 더 빠름
-
-            {
-                for (int i = 0; i < 10000; i++)
-                    for (int j = 0; j < 10000; j++) arr[j, i] = 1;
-            }
+            Console.WriteLine($"{count}번 만에 빠져나옴");
         }
     }
-    
 }
