5a58f80cd57333e0aaa36414110c54eaf4d03995
THISISJUSTICE <yulop64@naver.com>
2023-12-07 12:41:49 +0900

    컴파일러 최적
    Debug 모드가 아닌 Release모드가 되면 코드가 최적화되면서 멀티 쓰레드 환경에서는 오류가 발생할 수 있음

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,53 +4,24 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
-    class Program
-    {
-        //메모리 배리어
-        //- 코드 재배치 억제
-        //- 가시성
-        static int x = 0;
-        static int y = 0;
-        static int r1 = 0;
-        static int r2 = 0;
-        static int a;
-
-        static void Thread1() {
-            y = 1; //Store y
-
-            //-----------------------------
-            Thread.MemoryBarrier(); //멀티 스레드에서만 적용된 값을 커밋하는 역할도 겸함(Store의 커밋)
-            r1 = x; //Load x
-        }
-
-        static void Thread2()
+    class Program {
+        static bool stop = false;
+        static void Thread1()
         {
-            x = 1; //Store x
+            Console.WriteLine("쓰레드 시작");
+            while(!stop){
 
-            //-----------------------------
-            Thread.MemoryBarrier();
-            r2 = y; //Load y
+            }
+            Console.WriteLine("쓰레드 끝");
         }
 
         static void Main(string[] args)
         {
-            int count = 0;
-            while (true) {
-                count++;
-                x = y = r1 = r2 = 0;
-
-                Task t1 = new Task(Thread1);
-                Task t2 = new Task(Thread2);
-                t1.Start();
-                t2.Start();
-
-                Task.WaitAll(t1, t2);
-
-                //메모리 배리어를 하지 않으면 코드의 순서가 바뀌는 경우가 있을 수 있어 해당 사항이 나올 수 있음
-                if (r1 == 0 && r2 == 0) break;
-            }
-
-            Console.WriteLine($"{count}번 만에 빠져나옴");
+            Task t = new Task(Thread1);
+            t.Start();
+            stop = true;
+            //Thread1의 무한 반복이 끝나지 않게 됨
         }
     }
+    
 }
